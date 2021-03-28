9ad9c9492e4ee9bc1cbada2a05c16bcab32de1e8
tushar9604 <tushar9604@gmail.com>
2021-03-28 20:20:38 +0530

    after Abstraction

---
+++ Abstraction/TestAbstraction1.java
@@ -0,0 +1,20 @@
+abstract class Shape{
+	abstract void draw();
+}
+
+class Rectangle extends Shape{
+	void draw(){System.out.println("drawing rectange");}
+}
+
+class Circle1 extends Shape{
+	void draw(){System.out.println("drawing circle");}
+}
+
+// IN real scenario, method is called by programmer or user
+
+class TestAbstraction1{
+	public static void main(String[] args){
+		Shape s = new Circle1(); // In a real scenario, object is provided through method, e.g., getShape() method
+		s.draw();
+	}
+}
