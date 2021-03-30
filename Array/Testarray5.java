11f24938654ef189e469010d432c169c26ba66a7
tushar9604 <tushar9604@gmail.com>
2021-03-30 18:42:44 +0530

    after Arrays

---
+++ Array/Testarray5.java
@@ -0,0 +1,13 @@
+// Java Program to get the class name of array in Java
+class Testarray5{
+	public static void main(String[] args){
+		// declaration and initialization of array
+		int arr[] = {4,4,5};
+		// getting the class name of Java array
+		Class c = arr.getClass();
+		String name = c.getName();
+		// printing the class name of Java array
+		System.out.println(name);
+	}
+}
+		
