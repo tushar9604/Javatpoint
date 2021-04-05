f325c2a1373394514f9ee9ed60f15e0c62f6cea2
tushar9604 <tushar9604@gmail.com>
2021-04-05 10:26:17 +0530

    after exceptions

---
+++ Exceptions/TestExceptionPropagation2.java
@@ -0,0 +1,21 @@
+class TestExceptionPropagation2{
+	void m(){
+		throw new java.io.IOException("device error"); // check exception
+	}
+	
+	void n(){
+		m();
+	}
+	
+	void p(){
+		try{
+		n();
+		} catch(Exception e){System.out.println("exception handeled");}
+	}
+	
+	public static void main(String[] args){
+		TestExceptionPropagation2 obj = new TestExceptionPropagation2();
+		obj.p();
+		System.out.println("normal flow");
+	}
+}
---
+++ Exceptions/TestThrows4.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class M{
+	void method() throws IOException{
+		throw new IOException("device error");
+	}
+}
+
+class TestThrows4{
+	public static void main(String[] args) throws IOException{//declare exception
+	M m = new M();
+	m.method();
+	
+	System.out.println("normal flow...");
+	}
+}
